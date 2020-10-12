ee0cefc007382a76f771a2cff67956de6a2df9e1
Gilmar Souza <gilmarcs@gmail.com>
2020-10-11 23:57:29 -0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,7 +3,7 @@
 #add mono repository (raspbian 8 - jessie - works for osmc) - required for jackett
 #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 #echo "deb http://download.mono-project.com/repo/debian raspbianjessie main" | sudo tee /etc/apt/sources.list.d/mono-official.list
-#sudo apt-get update
+sudo apt-get update && sudo apt-get dist-upgrade -y
 
 #install mono - required for jackett
 #sudo apt-get install mono-devel -y

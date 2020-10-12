71eb65b5cfa4aeb98eeb6baedab44835b8980c57
Gilmar Souza <gilmarcs@gmail.com>
2020-10-11 23:47:20 -0300

    Create install_couchpotato.sh

---
+++ couchpotato/install_couchpotato.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#remove old installation
+rm -Rf /home/osmc/Install/couchpotato
+
+#clone the repo
+git clone https://github.com/CouchPotato/CouchPotatoServer.git /home/osmc/Install/couchpotato
+
+#copy the configuration
+cp couchpotato_settings.conf /home/osmc/Install/couchpotato/settings.conf
+
+#setup the service
+sudo cp couchpotato_defaults /etc/default/couchpotato
+sudo cp /home/osmc/Install/couchpotato/init/ubuntu /etc/init.d/couchpotato
+sudo chmod +x /etc/init.d/couchpotato
+
+sudo chmod +x /etc/default/couchpotato
+sudo update-rc.d couchpotato defaults

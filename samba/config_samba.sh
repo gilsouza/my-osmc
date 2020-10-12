dabd1e8b27eb6ca065bca2a8f7fc3c4a3a7f6d7c
Gilmar Souza <gilmarcs@gmail.com>
2020-10-12 01:23:30 -0300

    Update config_samba.sh

--- samba/config_samba.sh
+++ samba/config_samba.sh
@@ -8,6 +8,6 @@ sudo useradd –G osmc gabi
 sudo smbpasswd -a gil
 sudo smbpasswd -a gabi
 
-cp smb-shares.conf /etc/samba/smb-shares.conf
+sudo cp smb-shares.conf /etc/samba/smb-shares.conf
 
 sudo service smbd restart

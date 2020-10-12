c03d647340273e1775f87c8ed728dfb2b024135f
Gilmar Souza <gilmarcs@gmail.com>
2020-10-11 23:51:48 -0300

    Create configure_transmission.sh

---
+++ transmission/configure_transmission.sh
@@ -0,0 +1,7 @@
+  
+#!/bin/bash
+
+sudo service transmission stop
+cp transmission_settings.json /home/osmc/.config/transmission-daemon/settings.json
+cp torrent-done.sh /home/osmc/Downloads/torrent-done.sh
+sudo service transmission start

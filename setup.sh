094af540e395a0fd401838ef4983f1df03d1da2c
Gilmar Souza <gilmarcs@gmail.com>
2020-10-11 23:44:32 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+
+./install_dependencies.sh
+
+cd transmission
+./configure_transmission.sh
+cd ..
+
+cd couchpotato
+./install_couchpotato.sh
+cd .. 
+
+cd sickchill
+./install_sickchill.sh
+cd ..

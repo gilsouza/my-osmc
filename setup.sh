45c714027bc6dd1b1fad24d844379f5a9f571fdf
Gilmar Souza <gilmarcs@gmail.com>
2020-10-14 09:04:32 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+cd basic
+./setup_basic.sh
+cd ..
+
 ./install_dependencies.sh
 
 cd transmission
 ./configure_transmission.sh
 cd ..
 
-cd couchpotato
-./install_couchpotato.sh
-cd .. 
+#cd couchpotato
+#./install_couchpotato.sh
+#cd .. 
 
 #cd sickchill
 #./install_sickchill.sh

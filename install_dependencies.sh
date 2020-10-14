383246824ae9149de05af413f3c822d830981007
Gilmar Souza <gilmarcs@gmail.com>
2020-10-14 09:01:59 -0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-mkdir -p media/drive
+mkdir /mnt/drive
+#montar drive em /mnt/drive/
+
+ln -s /mnt/drive/SMB .
+rm -rf Movies
+ln -s /mnt/drive/Movies/ .
+rm -rf Music
+ln -s /mnt/drive/Music/ .
+rm -rf Pictures/
+ln -s /mnt/drive/Pictures/ .
+rm -rf TV\ Shows/
+ln -s /mnt/drive/TV\ Shows/ .
+
+sudo chown -R osmc:osmc /mnt/drive/Movies/ /mnt/drive/Music/ /mnt/drive/Pictures/ /mnt/drive/TV\ Shows/
 
 sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt-get install git

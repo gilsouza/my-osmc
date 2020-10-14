27d834271fd52e3328e1ccbb9f36edccf9ec7f87
Gilmar Souza <gilmarcs@gmail.com>
2020-10-14 09:03:24 -0300

    Create setup_basic.sh

---
+++ basic/setup_basic.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+mkdir /mnt/drive
+#montar drive em /mnt/drive/
+
+ln -s /mnt/drive/SMB .
+rm -rf Movies
+ln -s /mnt/drive/Movies/ .
+rm -rf Music
+ln -s /mnt/drive/Music/ .
+rm -rf Pictures/
+ln -s /mnt/drive/Pictures/ .
+rm -rf TV\ Shows/
+ln -s /mnt/drive/TV\ Shows/ .
+
+sudo chown -R osmc:osmc /mnt/drive/Movies/ /mnt/drive/Music/ /mnt/drive/Pictures/ /mnt/drive/TV\ Shows/

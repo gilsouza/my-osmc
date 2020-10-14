79987b0c678018a5db9e1ff85f699dee13c40d05
Gilmar Souza <gilmarcs@gmail.com>
2020-10-14 08:14:00 -0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-#add mono repository (raspbian 8 - jessie - works for osmc) - required for jackett
-#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-#echo "deb http://download.mono-project.com/repo/debian raspbianjessie main" | sudo tee /etc/apt/sources.list.d/mono-official.list
+mkdir -p media/drive
+
 sudo apt-get update && sudo apt-get dist-upgrade -y
-
-#install mono - required for jackett
-#sudo apt-get install mono-devel -y
-
-#install p7zip for extraction zip files
-sudo apt-get install p7zip-full -y
-
-#install unrar
-sudo apt-get install unrar -y
-
-#install some other dependencies
-sudo apt-get install python-cheetah git-core libcurl4-openssl-dev jq nodejs -y
-
-#install sickrage specific dependencies
-sudo apt-get install python-setuptools build-essential python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev -y
+sudo apt-get install git
+sudo git clone https://github.com/htpcBeginner/AtoMiC-ToolKit /opt/AtoMiC-ToolKit
+cd /opt/AtoMiC-ToolKit
+sudo bash setup.sh -i couchpotato
+sudo bash setup.sh -i radarr
+sudo bash setup.sh -i sonarr
+sudo bash setup.sh -i sickrage
+sudo bash setup.sh -i jackett

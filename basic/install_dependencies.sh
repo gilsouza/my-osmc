b4152250b1afb29db097579ee7bca332e745efcd
Gilmar Souza <gilmarcs@gmail.com>
2020-10-15 20:52:12 -0300

    Update install_dependencies.sh

--- basic/install_dependencies.sh
+++ basic/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get dist-upgrade -y
-sudo apt-get install git libsqlite3-dev libbz2-dev libreadline-dev zlib1g-dev libxslt1-dev libxml2-dev libmono-cil-dev
+sudo apt-get install git libsqlite3-dev libbz2-dev libreadline-dev zlib1g-dev libxslt1-dev libxml2-dev libmono-cil-dev jq

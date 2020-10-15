bc17f568df01bfe56b009bf6496d73f48c11bfc8
Gilmar Souza <gilmarcs@gmail.com>
2020-10-15 19:25:41 -0300

    Update torrent-done.sh

--- transmission/torrent-done.sh
+++ transmission/torrent-done.sh
@@ -8,6 +8,3 @@ wget -qO /dev/null http://localhost:8081/api/$SR_API_KEY/?cmd=postprocess
 
 #trigger couchpotato post processing for Movies dir
 wget -qO /dev/null http://localhost:5050/api/$CP_API_KEY/renamer.scan
-
-#move MoviesBG
-mv /home/osmc/Downloads/MoviesBG/* /home/osmc/MoviesBG/

6b6cc9622af846cf0b98aa7c7d9362bcf4849396
Gilmar Souza <gilmarcs@gmail.com>
2020-10-15 19:55:24 -0300

    Create install_sickrage.sh

---
+++ sickrage/install_sickrage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#stop service
+sudo service sickrage stop
+
+#remove old installation
+rm -Rf /home/osmc/Install/sickrage
+
+#clone the repo
+git clone https://github.com/SickRage/SickRage.git /home/osmc/Install/sickrage
+
+#copy the preloaded configuration
+cp sickrage_config.ini /home/osmc/Install/sickrage/config.ini
+
+#setup the service
+sudo cp sickrage_defaults /etc/default/sickrage
+sudo cp /home/osmc/Install/sickrage/runscripts/init.ubuntu /etc/init.d/sickrage
+sudo cp sickrage-app-osmc /etc/osmc/apps.d/sickrage-app-osmc
+sudo chmod +x /etc/init.d/sickrage
+
+sudo chmod +x /etc/default/sickrage
+sudo update-rc.d sickrage defaults
+
+#start service
+sudo service sickrage start

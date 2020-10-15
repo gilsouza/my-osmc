553fc4ecb527e9f3b8711b1d7243c4efc7ad6249
Gilmar Souza <gilmarcs@gmail.com>
2020-10-15 19:21:43 -0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,10 +2,3 @@
 
 sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt-get install git
-sudo git clone https://github.com/htpcBeginner/AtoMiC-ToolKit /opt/AtoMiC-ToolKit
-cd /opt/AtoMiC-ToolKit
-sudo bash setup.sh -i couchpotato
-sudo bash setup.sh -i radarr
-sudo bash setup.sh -i sonarr
-sudo bash setup.sh -i sickrage
-sudo bash setup.sh -i jackett

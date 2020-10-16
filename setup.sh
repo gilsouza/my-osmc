5c4be8a17687e0e9c16adef652e7792c22a4bb29
Gilmar Souza <gilmarcs@gmail.com>
2020-10-16 08:39:10 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+function usage
+{
+    echo "Description: Setup the OSMC"
+    echo "Usage: setup.sh <media storage path>"
+}
+
+function fail
+{
+    echo ""
+    echo "=> $1" 1>&2
+    echo ""
+    usage
+    echo ""
+    exit 1
+}
+
+MEDIA_STORAGE=$1
+
+[ -n "$MEDIA_STORAGE" ] || fail "No media storage path provided"
+
+mkdir /mnt/drive
+
+if ! grep -q 'init-drive' /etc/fstab ; then
+    echo '# init-drive' >> /etc/fstab
+    echo 'UUID=$MEDIA_STORAGE	/mnt/drive	auto	rw,user,auto	0	0' >> /etc/fstab
+fi
+
+sudo mount -U $MEDIA_STORAGE /mnt/drive
+
 cd basic
 ./setup_basic.sh
 ./install_dependencies.sh

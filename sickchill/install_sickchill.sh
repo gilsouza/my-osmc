67a746c450a36fdc026938fdd5c4f4c6aa9b5e19
Gilmar Souza <gilmarcs@gmail.com>
2020-10-15 22:53:53 -0300

    Update install_sickchill.sh

--- sickchill/install_sickchill.sh
+++ sickchill/install_sickchill.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+#stop service
+sudo service sickchill stop
+
+#remove old installation
+rm -Rf /home/osmc/Install/sickchill
+
+#clone the repo
+git clone https://github.com/SickChill/SickChill.git /home/osmc/Install/sickchill
+
+#copy the preloaded configuration
+cp sickchill_config.ini /home/osmc/Install/sickchill/config.ini
+
+#setup the service
+sudo cp sickchill_defaults /etc/default/sickchill
+sudo cp /home/osmc/Install/sickchill/runscripts/init.ubuntu /etc/init.d/sickchill
+sudo chmod +x /etc/init.d/sickchill
+
+sudo chmod +x /etc/default/sickchill
+sudo update-rc.d sickchill defaults
+
+#start service
+sudo service sickrage start
